52e0613824142aae3e49b3bb27d4843146ac897e
Adarsh kumar kuiya <adarshk9889@gmail.com>
2022-12-31 12:07:39 +0530

    bank

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,9 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+    super(name,balance);
+    this.tradeLicenseId=tradeLicenseId;
+    if(getBalance()<5000) throw new Exception("Insufficient Balance");
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +15,50 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
+    boolean check=false;
+    for(int i=0;i<this.tradeLicenseId.length()-1;i++){
+    if(this.tradeLicenseId.charAt(i)==this.tradeLicenseId.charAt(i+1)){
+    check=true;
+    break;
+            }
+        }
+    if(check){
+     String s= Rearrange(this.tradeLicenseId);
+    if(s.equals(""))throw new Exception("Valid License can not be generated");
+    else{
+     this.tradeLicenseId=s;
     }
+        }
+    }
+    public String Rearrange(String s){
+        int n = s.length(), maxFreq = 0, maxLetter = -1;
+        int[] map = new int[26];
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            map[c] += 1;
+            if (map[c] > maxFreq) {
+                maxFreq = map[c];
+                maxLetter = c;
+            }
+        }
 
+        if (maxFreq - 1 > n - maxFreq) return "";
+        char[] res = new char[n];
+        int index = 0;
+        while (map[maxLetter] != 0) {
+            res[index] = (char) ('a' + maxLetter);
+            map[maxLetter] -= 1;
+            index += 2;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (map[i] != 0) {
+                if (index >= n) index = 1;
+                res[index] = (char) ('a' + i);
+                map[i] -= 1;
+                index += 2;
+            }
+        }
+        return new String(res);
+    }
 }
